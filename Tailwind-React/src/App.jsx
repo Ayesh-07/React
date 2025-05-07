49da66396c703f6eb9e8f4e5a410a110c578342d
Ayesh-07 <ayeshnud@gmail.com>
2025-05-07 13:44:31 +0500

    install Tailwind

---
+++ Tailwind-React/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Components/Header.jsx'
+import './index.css'
+
+const App = () => {
+  return (
+    <>
+
+<Header/>
+
+    </>
+    
+  )
+}
+
+export default App
